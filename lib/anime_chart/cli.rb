c09840013e12cbccc803f118dc52284b24c53f66
Laura Felce <crepu83@yahoo.it>
2019-02-01 14:59:10 +0000

    defining list_chart method

--- lib/anime_chart/cli.rb
+++ lib/anime_chart/cli.rb
@@ -1,6 +1,13 @@
 class AnimeChart::CLI
   
+  def call
+    list_chart
+    #anime_details
+    #goodbye
+  end
   
+  def list_chart
+    puts "Welcome to Anime Chart! If you want to check the 50 top anime according to MyAnimeList type list. No time to read the entire list? Type a number between 1 and 50 to check which anime is currently holding that position."
+  end  
   
-end  
-  
+end
