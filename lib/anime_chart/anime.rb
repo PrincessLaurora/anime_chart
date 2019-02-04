7e649884b1467a0cf6919e49c8685ebee421cc27
Laura Felce <crepu83@yahoo.it>
2019-02-04 17:18:45 +0000

    working scrape_list- name

--- lib/anime_chart/anime.rb
+++ lib/anime_chart/anime.rb
@@ -3,30 +3,25 @@ class AnimeChart::Anime
   attr_accessor :name, :score, :details
   
   
- 
-  def self.scrape_list
+ def self.scrape_list
     
     doc = Nokogiri::HTML(open("https://myanimelist.net/topanime.php?limit=0"))
     
-    array = []
+    chart = []
     
-    anime = self.new  
-    
-    doc.css(".di-ib.clearfix").each do |list|
+    doc.css("div.di-ib.clearfix").each do |list|
+      
+    anime = self.new
     
     anime.name = list.css("a.hoverinfo_trigger.fl-l.fs14.fw-b").text
-    
-    
-    array << anime
-    
-    array
-      #binding.pry
+     
+    chart << anime
     end
-  
-  end
-  
-
     
+    chart
+    #binding.pry
+  end
+
   
 
 
--- lib/anime_chart/cli.rb
+++ lib/anime_chart/cli.rb
@@ -8,9 +8,9 @@ class AnimeChart::CLI
   
   def list_chart
     puts "Welcome to Anime Chart! If you want to check the 50 top anime according to MyAnimeList type list. No time to read the entire list? Type a number between 1 and 50 to check which anime is currently holding that position."
-    @array = AnimeChart::Anime.scrape_list
-    @array.each.with_index(1) do |anime, i|
-      puts "#{i} - #{anime.name}"
+    @chart = AnimeChart::Anime.scrape_list
+    @chart.each.with_index(1) do |anime, index|
+      puts "#{index}) #{anime.name}"
     end
   end  
   
