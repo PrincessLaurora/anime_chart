c67338dc4ee911a6f0b3a04d137021ec1d1d90e4
Laura Felce <crepu83@yahoo.it>
2019-02-04 13:01:06 +0000

    scrape_list test3

--- lib/anime_chart/anime.rb
+++ lib/anime_chart/anime.rb
@@ -3,17 +3,6 @@ class AnimeChart::Anime
   attr_accessor :name, :score, :details
   
   
-
-  #def self.anime_list
-    
-    #chart = []
-    
-    #chart << self.scrape_list
-    
-    #chart
-  
-  #end
-  
  
   def self.scrape_list
     
@@ -23,22 +12,20 @@ class AnimeChart::Anime
     
     anime = self.new  
     
-    #content = doc.css("div.pb12")
     doc.css(".di-ib.clearfix").each do |list|
     
     anime.name = list.css("a.hoverinfo_trigger.fl-l.fs14.fw-b").text
-  
+    
     
     array << anime
     
     array
-      
-    
-  end
+      #binding.pry
     end
   
+  end
   
-  
+
     
   
 
--- lib/anime_chart/cli.rb
+++ lib/anime_chart/cli.rb
@@ -8,8 +8,8 @@ class AnimeChart::CLI
   
   def list_chart
     puts "Welcome to Anime Chart! If you want to check the 50 top anime according to MyAnimeList type list. No time to read the entire list? Type a number between 1 and 50 to check which anime is currently holding that position."
-    @chart = AnimeChart::Anime.scrape_list
-    @chart.each.with_index(1) do |anime, i|
+    @array = AnimeChart::Anime.scrape_list
+    @array.each.with_index(1) do |anime, i|
       puts "#{i} - #{anime.name}"
     end
   end  
