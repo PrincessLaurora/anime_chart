39bf06aaa791aea86f4e6227839bc848e283f92b
Laura Felce <crepu83@yahoo.it>
2019-02-04 12:32:21 +0000

    scrape_list test2

--- lib/anime_chart/anime.rb
+++ lib/anime_chart/anime.rb
@@ -2,35 +2,45 @@ class AnimeChart::Anime
   
   attr_accessor :name, :score, :details
   
-  def self.get_page
-    Nokogiri::HTML(open("https://myanimelist.net/topanime.php?limit=0"))
-  end  
+  
 
+  #def self.anime_list
+    
+    #chart = []
+    
+    #chart << self.scrape_list
+    
+    #chart
+  
+  #end
   
  
   def self.scrape_list
     
-    content = self.get_page.css("div #content .pb12")
-    content.css(".ranking-list").each do |list|
+    doc = Nokogiri::HTML(open("https://myanimelist.net/topanime.php?limit=0"))
+    
+    array = []
     
     anime = self.new  
     
-    anime.name = list.css("div.di-ib.clearfix a.hoverinfo_trigger.fl-l.fs14.fw-b").text
+    #content = doc.css("div.pb12")
+    doc.css(".di-ib.clearfix").each do |list|
     
-    anime
-    #binding.pry
+    anime.name = list.css("a.hoverinfo_trigger.fl-l.fs14.fw-b").text
   
-end
+    
+    array << anime
+    
+    array
+      
+    
   end
+    end
+  
+  
   
     
   
-  def self.list
-    
-    chart = []
-    chart << self.scrape_list
-    chart
-  end
-  
+
 
 end
--- lib/anime_chart/cli.rb
+++ lib/anime_chart/cli.rb
@@ -8,7 +8,7 @@ class AnimeChart::CLI
   
   def list_chart
     puts "Welcome to Anime Chart! If you want to check the 50 top anime according to MyAnimeList type list. No time to read the entire list? Type a number between 1 and 50 to check which anime is currently holding that position."
-    @chart = AnimeChart::Anime.list
+    @chart = AnimeChart::Anime.scrape_list
     @chart.each.with_index(1) do |anime, i|
       puts "#{i} - #{anime.name}"
     end
