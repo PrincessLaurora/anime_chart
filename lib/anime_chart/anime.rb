de70768ba029f86b68e73deac057ed4abd3bea41
Laura Felce <crepu83@yahoo.it>
2019-02-04 10:47:47 +0000

    anime name

--- lib/anime_chart/anime.rb
+++ lib/anime_chart/anime.rb
@@ -1,27 +1,36 @@
 class AnimeChart::Anime
   
-  attr_accessor :rank, :name, :score, :details
+  attr_accessor :name, :score, :details
   
-  def self.chart
+  def self.get_page
+    Nokogiri::HTML(open("https://myanimelist.net/topanime.php?limit=0"))
+  end  
+
+  
+ 
+  def self.scrape_list
+    
+    content = self.get_page.css("div #content .pb12")
+    content.css(".ranking-list").each do |list|
+    
+    anime = self.new  
+    
+    anime.name = list.css("div.di-ib.clearfix a.hoverinfo_trigger.fl-l.fs14.fw-b").text
+    
+    anime
+    #binding.pry
+  
+end
+  end
+  
+    
+  
+  def self.list
     
     chart = []
     chart << self.scrape_list
     chart
   end
-  
-  
-  def self.scrape_list
-    
-    doc = Nokogiri::HTML(open("https://myanimelist.net/topanime.php?limit=0"))
-    
-    game = self.new
-    
-    game.rank = doc.css(".rank.ac").text
-    
-    game
-    binding.pry
-  
-    end  
   
 
 end
--- lib/anime_chart/cli.rb
+++ lib/anime_chart/cli.rb
@@ -8,9 +8,9 @@ class AnimeChart::CLI
   
   def list_chart
     puts "Welcome to Anime Chart! If you want to check the 50 top anime according to MyAnimeList type list. No time to read the entire list? Type a number between 1 and 50 to check which anime is currently holding that position."
-    @chart = AnimeChart::Anime.chart
-    @chart.each do |game|
-      puts "#{game.rank}"
+    @chart = AnimeChart::Anime.list
+    @chart.each.with_index(1) do |anime, i|
+      puts "#{i} - #{anime.name}"
     end
   end  
   
