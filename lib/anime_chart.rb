14ab86ab3f4920563b37b94b16a1eef4dc49650b
Laura Felce <crepu83@yahoo.it>
2019-02-05 12:21:58 +0000

    CLI list_chart and list building, input and answer

--- lib/anime_chart.rb
+++ lib/anime_chart.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'colorize'
 
 require_relative "./anime_chart/version"
 require_relative './anime_chart/anime'
--- lib/anime_chart/cli.rb
+++ lib/anime_chart/cli.rb
@@ -2,17 +2,39 @@ class AnimeChart::CLI
   
   def call
     list_chart
-    #anime_details
+    anime_details
     #goodbye
   end
   
   def list_chart
-    puts "Welcome to Anime Chart! If you want to check the 50 top anime according to MyAnimeList type list. No time to read the entire list? Type a number between 1 and 50 to check which anime is currently holding that position."
-    @chart = AnimeChart::Anime.scrape_list
-    @chart.each.with_index(1) do |anime, index|
-      puts "#{index}) #{anime.name}   Score: #{anime.score}"
+    puts "Welcome to Anime Chart! If you want to check the 50 top anime by MyAnimeList, type list. 
+    No time to read the entire list? Type a number between 1 and 50 to check which anime is currently holding that position.
+    To exit the program type exit."
+     input = nil
+    while input != "exit"
+      input = gets.strip
+      if input == "list"
+      list
+      elsif input.to_i > 0
+     puts "te lo dico dopo"
+    else 
+      puts "salvati"
+    end  
+  end
+end
+  
+    def list
+      puts " "
+      puts " "
+      @chart = AnimeChart::Anime.scrape_list
+      puts "******************************TOP 50 ANIME*********************************".colorize(:blue)
+      @chart.each.with_index(1) do |anime, index|
+      puts " "
+      puts "#{index}) #{anime.name} ===> SCORE: #{anime.score}".colorize(:yellow)
+      end
     end
-  end  
+    
 
   
 end
+
